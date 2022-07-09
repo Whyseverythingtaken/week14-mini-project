07578275c74ded0ab93341fe01bf4d389b666ae1
Hamed Ali <123hamed@gmail.com>
2022-07-09 13:45:23 +0100

    connect login page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,8 +17,10 @@ router.post('/', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
+  console.log('Request body', req.body);
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
+    console.log('User Data', userData);
 
     if (!userData) {
       res
@@ -39,7 +41,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,24 @@
-const handleLogin = (event) => {
+const handleLogin = async (event) => {
   event.preventDefault();
-  alert('Login successful');
+
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
+
+  const response = await fetch('/api/users/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      email,
+      password
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Incorrect email and password combination');
+  }
 };
 
 document.getElementById('login-form').addEventListener('submit', handleLogin);
