10d8a9a881e5a67fe0401aa5345d251e66910b68
Hamed Ali <123hamed@gmail.com>
2022-07-09 13:15:47 +0100

    add views + connect handlebars

---
+++ controllers/homeRoutes/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const Projects = require('../../models/Project');
+
+router.get('/', async (req, res) => {
+  try {
+    const projects = await Projects.findAll({
+      raw: true
+    });
+    res.render('projects', {projects});
+  } catch (error) {
+    res.status(500).json(error);
+  }
+  
+});
+
+module.exports = router;
